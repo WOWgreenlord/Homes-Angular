92ae8db10ae9cbcf64d18b1342e2eaee47ff3725
Misha Dyakov <dock.misha@gmail.com>
2024-08-15 18:41:42 +0300

    initial commit;

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,19 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: `
+    <main>
+      <header class="brand-name">
+        <img
+          class="brand-logo"
+          src="/assets/logo.svg"
+          alt="logo"
+          aria-hidden="true"
+        />
+      </header>
+    </main>
+  `,
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Homes';
