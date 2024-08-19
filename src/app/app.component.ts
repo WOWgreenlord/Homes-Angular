4e42dfece12b190bceb9fc97380d2236a5b11727
Misha Dyakov <dock.misha@gmail.com>
2024-08-19 20:09:10 +0300

    edit and add: add components and styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent, CommonModule],
   template: `
     <main>
       <header class="brand-name">
@@ -15,6 +17,9 @@ import { RouterOutlet } from '@angular/router';
           aria-hidden="true"
         />
       </header>
+      <section class="conetnt">
+        <app-home></app-home>
+      </section>
     </main>
   `,
   styleUrl: './app.component.css',
